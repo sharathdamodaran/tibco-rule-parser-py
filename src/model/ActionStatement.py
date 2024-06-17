1c382ae9da59c6b9258e3ea873e78677ea8e1452
Sharath Damodaran <sharath.damodaran@thoughtworks.com>
2024-06-17 08:50:39 +0530

    Defined the action class for holding action/matrix  and parsing them from a statement

---
+++ src/model/ActionStatement.py
@@ -0,0 +1,26 @@
+class ActionStatement:
+    def __init__(self, statement=None, values=None):
+        self.statement = statement
+        self.values = values if values is not None else []
+    
+    @classmethod
+    def create_or_update(cls, action_statement, prev_action):
+        statements = action_statement.split("¦")
+        action_matrix_values = []
+        if len(statements) == 1 or not statements[1].strip():
+            prev_action.statement=prev_action.statement + "\n" + statements[0]
+            return False, prev_action
+        else:
+            for i in range(1, len(statements[1]), 2):
+                action_matrix_values.append(
+                    cls.convert_action_value_to_boolean(statements[1][i])
+                )
+            return True, cls(statements[0],action_matrix_values)
+    
+    @staticmethod    
+    def convert_action_value_to_boolean(value):
+        if value.isdigit():
+            return True
+        else:
+            return False
+    
